c758f11c388561f476b9a2bc5f37bf808b6bed33
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2022-10-29 17:14:16 +0200

    update app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from here</p>
-        <Weather city="Tokyo" />
+        <Weather />
       </header>
     </div>
   );
--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-export default function Weather(props) {
-  function showTemperature(response) {
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function handleWeather(response) {
     console.log(response.data);
-    alert(
-      `The temperature in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )} C`
-    );
+    setLoaded(true);
+
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+    });
   }
 
-  let apiKey = "2af1ab046f283f38f8ddb93800aaf9bd";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = `2af1ab046f283f38f8ddb93800aaf9bd`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    console.log(url);
+    axios.get(url).then(handleWeather);
+    //access to the city
+    //alert(city);
+    //1.API call
+    //2.update the weather UI
+  }
 
-  axios.get(url).then(showTemperature);
-  return <h1> Hello there </h1>;
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <h1> Weather App </h1>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={handleCity} />
+        <input type="submit" value="Search" />
+      </form>
+    </div>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li> Temperature {Math.round(weather.temperature)} °C</li>
+          <li> Description: {weather.description} </li>
+          <li> Humidity {weather.humidity}%</li>
+          <li>Wind {weather.wind} km/h</li>
+
+          <ReactAnimatedWeather
+            icon="CLEAR_DAY"
+            color="goldenrod"
+            size={30}
+            animate={true}
+          />
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
